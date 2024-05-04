799e2e3443caf27582e44e52af2c8c10239ba628
Stivliz <stivliz0909@gmail.com>
2024-05-04 10:32:53 -0500

    feat: conectiion to DB

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config'
 import app from './app'
-
+import dbConnect from './database';
 const PORT = process.env.PORT || 3001;
+
+dbConnect().then(() => console.log('server conected'))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`)
