7b551a28ccf36b62eabc40b460c03f0bb9b38f79
Stivliz <stivliz0909@gmail.com>
2024-05-04 00:56:34 -0500

    feat: create config backend-mc

---
+++ backend-mc/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import cors from "cors"
+import morgan from "morgan";
+
+const app = express()
+
+//Middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors())
+
+export default app;
