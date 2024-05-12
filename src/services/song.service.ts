e381b5f9daea365b9f5a73fdb7b1404565a49881
Stivliz <stivliz0909@gmail.com>
2024-05-11 20:30:26 -0500

    feat: editing of information in the song model services

--- src/services/song.service.ts
+++ src/services/song.service.ts
@@ -2,17 +2,12 @@ import SongModel from "../models/song.model";
 import ISong from "../interfaces/interfaces";
 import { createCaseInsensitiveRegex } from "../utils/helpers/createCaseInsensitiveRegex";
 
-const insertSong = async (song:ISong) => {
+export const insertSong = async (song:ISong) => {
     const responseInsert = await SongModel.create(song);
     return responseInsert
 }
 
-const  findSongs = async (name:ISong) => {
+export const  findSongs = async (name:ISong) => {
     const searchResponse = await SongModel.find({ name: createCaseInsensitiveRegex(name.name) })
     return searchResponse;
-}
-
-export default {
-    insertSong,
-    findSongs
 }
