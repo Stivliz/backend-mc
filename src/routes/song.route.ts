e2920ccbb1e33ccf8864d7600cd87175cc68bc1c
Stivliz <stivliz0909@gmail.com>
2024-05-11 22:12:54 -0500

    feat: new changes in the back, routes, services, controllers...

--- src/routes/song.route.ts
+++ src/routes/song.route.ts
@@ -4,7 +4,7 @@ import songControllers from '../controllers/song.controllers'
 const router = Router();
  
 router.get('/songs', songControllers.getSongs)
-      .get('/song/:id', songControllers.getSong)
+      .get('/song', songControllers.getSong)
       .post('/song', songControllers.postSong)
       .put('/song/:id', songControllers.updateSong)
       .delete('/song/:id', songControllers.deleteSong)
